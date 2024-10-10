cb1f16dc5f8f63f52d5e9c62fb9a4deb21901c03
ozgur <ozgur@merkezsiz.net>
2024-10-10 19:16:01 +0300

    initial gateway service

--- apps/gateway/lib/gateway/station_service.ex
+++ apps/gateway/lib/gateway/station_service.ex
@@ -19,13 +19,24 @@ defmodule Gateway.StationService do
     send_resp(conn, 200, "Success!")
   end
 
-  post "/status" do
+  post "/status_update" do
     # Station periodically sends its status updates so corresponding Station updates its state
     # Message format {station_id : string, stasus: list}
-    #example {station_id: "1234", status: {battery_level}}
-
-    send_resp(conn, 200, "Station #{conn.body_params["battery_level"]}")
+    #Message Protocol
+    #StationId: 128 bit
+    #Message type 8 bit = 256 different message types
+    #Message payload variable size
+    #example {station_id: "1234", changes: [
+    #battery_level_changed: 50,
+    #device_battery_level_changed: {id: 1,  battery_level: 100
+    #devices should not be sent as a status update as there can be 100 device attached to a station
+    #so device status should be sent to different contract
+    #In that sense station should always send what has changed!!! Thats the responsibility of Station(hw)
+    #message will be passed to GenServer
+    IO.inspect conn.body_params # Prints JSON POST body
+    send_resp(conn, 200, "Station #{conn.body_params["status"]}")
   end
+
 
 
 
@@ -33,4 +44,5 @@ defmodule Gateway.StationService do
   match _ do
     send_resp(conn, 404, "Not Found")
   end
+
 end
--- apps/gateway/mix.exs
+++ apps/gateway/mix.exs
@@ -27,7 +27,8 @@ defmodule Gateway.MixProject do
   defp deps do
     [
       {:bandit, "~> 1.5"},
-      {:plug, "~> 1.16"}
+      {:plug, "~> 1.16"},
+      {:jason, "~> 1.4"}
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"},
       # {:sibling_app_in_umbrella, in_umbrella: true}
